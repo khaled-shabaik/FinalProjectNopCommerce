a6cd87561759f449e7193b202470c7d762e32817
khaled ramadan <khaled-shabaik@la3eb.com>
2025-05-14 00:13:46 +0300

    first commit

---
+++ src/test/java/org/example/base/base.java
@@ -0,0 +1,19 @@
+package org.example.base;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class base {
+    @BeforeMethod
+    public void openBrowser()
+    {
+
+    }
+
+    @AfterMethod
+    public void quitDrive()
+    {
+
+    }
+
+}
